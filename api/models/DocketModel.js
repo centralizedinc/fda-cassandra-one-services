02770e327c758b1507e791dd4eb710a9488e1829
Kris <blitzkris24@gmail.com>
2019-04-24 07:41:57 +0800

    cassandra

--- api/models/DocketModel.js
+++ api/models/DocketModel.js
@@ -7,6 +7,30 @@ var schema = new mongoose.Schema({
     created_date: Date,
     modified_by:String,
     modified_date: Date,
+    
+    //new docket
+
+    dtn : 0,
+    origin_cluster: String,
+    origin_region: String,
+    origin_area: String,
+    inspection_date: Date,
+    inspection_inspector: String,
+    inspection_purpose: String,
+    rov_date: Date,
+    rov_date_receive: Date,
+    rov_center_involved: String,
+    establishment_compliant: String,
+    establishment_name: String,
+    establishment_owner: String,
+    establishment_classification: String,
+    pharmacist_radiologist: String,
+    class_of_product: String,
+    establishment_address_region: String,
+    establishment_address_province: String,
+    establishment_address_city: String,
+    establishment_address: String,
+    documents: File
 })
 
 module.exports = mongoose.model("dockets", schema);
